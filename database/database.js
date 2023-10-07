1dfe39cffa295a343e7a1d76ad55977e58eb3cd2
Suprim Lal Amatya <suprim.amatya53@gmail.com>
2023-10-08 00:27:31 +0545

    feat: add external database.js file for export

--- database/database.js
+++ database/database.js
@@ -3,11 +3,8 @@ const mongoose = require("mongoose");
 exports.connectDatabase = async()=>{
     // connecting to database
     // jaba samma database sanga connect hudaena wait garr
-    
-    // await mongoose.connect("mongodb+srv://suprimamatya53:cmspassword@cluster0.saxpjhe.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp")
-    // console.log("Database connected successfully")
 
-    mongoose.connect("mongodb+srv://suprimamatya53:cmspassword@cluster0.saxpjhe.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp")
+    await mongoose.connect("mongodb+srv://user1:user1@cluster0.mfvljhb.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp")
     .then(()=>{
         console.log("Database connected successfully")
     })
